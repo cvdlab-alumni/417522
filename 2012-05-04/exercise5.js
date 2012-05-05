695498b22beb6847acfb5fc6255bb7f2cf37fd58
Marco Ungania <mar.ungania@gmail.com>
2012-05-05 19:01:49 +0200

    ex5

--- 2012-05-04/exercise5.js
+++ 2012-05-04/exercise5.js
@@ -2,7 +2,7 @@
 var wing2dDomain = INTERVALS(1)(30);
 var domain2 =  DOMAIN([[0,1],[0,1]])([36,36]);
 
-var grey = [0.7,0.7,0.7];
+var grey = [139/255,69/255,19/255];
 var black = [0,0,0];
 
 var l = 4; // larghezzaAla
