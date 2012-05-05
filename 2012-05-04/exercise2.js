5c630b2b19a6352475cd24cd7878eb3c098a0ba6
Marco Ungania <mar.ungania@gmail.com>
2012-05-05 11:53:48 +0200

    ex2

---
+++ 2012-05-04/exercise2.js
@@ -0,0 +1,64 @@
+
+var getBezierS0 = function (controls,n,draw){
+
+	var domain = INTERVALS(1)(n || 20);
+	var mapping = BEZIER(S0)(controls);
+
+	var c = MAP(mapping)(domain);
+	if(draw){
+		DRAW(c);
+	}
+
+	return c;
+}
+
+var drawBz0 = function(controls){
+	DRAW(getBezierS0(controls));
+}
+
+var domain2 =  DOMAIN([[0,1],[0,1]])([36,36]);
+
+// FUSOLIERA
+var r = 0.2; // raggio cerchio
+var cp0 = [ [0,-0.5,0], [0,-0.5,-0.2], [0,-0.3,-0.2], [0,-0.1,-0.2], [0,-0.1,0.2], [0,-0.3,0.2], [0,-0.5,0.2], [0,-0.5,0] ];
+cp0 = cp0.map(function (p){return [p[0]-0.2,p[1]*2,p[2]*2]});
+var c0 = BEZIER(S0)(cp0);
+
+var cp1 = [[0,-0.5,0], [0,-0.5,-0.5], [0,0,-0.5], [0.4,0.5,0], [0,0,0.5], [0,-0.5,0.5], [0,-0.5,0]];
+cp1 = cp1.map(function (p){return [p[0]*2,p[1]*2,p[2]*2]});
+var c1 = BEZIER(S0)(cp1);
+
+var y = 0.5; // aumento di y
+var z = 0.1; // aumento di z
+
+var cp2 = [[0,-0.5,0], [0,-0.5,-0.5-z], [0,0,-0.5-z], [0,0.5+y,0], [0,0,0.5+z], [0,-0.5,0.5], [0,-0.5,0]];
+cp2 = cp2.map(function (p){return [p[0]+1,p[1],p[2]]});
+var c2 = BEZIER(S0)(cp2);
+
+
+//var cp3 = [[0,-0.5,0], [0,-0.5,-0.5-z], [0,0,-0.5-z], [0,0.5+y,0], [0,0,0.5+z], [0,-0.5,0.5], [0,-0.5,0]];
+var cp3 = cp2.map(function (p){return [p[0]+1,p[1],p[2]]});
+var c3 = BEZIER(S0)(cp3);
+
+
+y = -0.3;
+z = -0.1;
+//var cp4 = [[0,-0.5,0], [0,-0.5,-0.5-z], [0,0,-0.5-z], [0,0.5+y,0], [0,0,0.5+z], [0,-0.5,0.5], [0,-0.5,0]];
+var cp4 = cp3.map(function (p){return [p[0]+1.5,p[1]/2,p[2]/2]});
+var c4 = BEZIER(S0)(cp4);
+
+cp5 = cp4.map(function (p){return [p[0]+1.5,p[1]/2,p[2]/2]});
+var c5 = BEZIER(S0)(cp5);
+
+
+var fusolieraMapping = BEZIER(S1)([c0,c1,c2,c3,c4,c5]);
+var fusoliera = MAP(fusolieraMapping)(domain2);
+
+DRAW(fusoliera);
+/*
+drawBz0(cp1);
+drawBz0(cp2);
+drawBz0(cp3);
+drawBz0(cp4);
+drawBz0(cp5);
+*/
